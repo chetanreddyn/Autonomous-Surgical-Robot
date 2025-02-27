f0d8a9c28d5a80c38fa393d70017b775d99bcd22
Chetan Reddy N <chetanreddy1412@gmail.com>
2025-02-26 22:15:31 -0800

    Added Clutch Functionality

--- teleop/scripts/phantom_teleop_latest.py
+++ teleop/scripts/phantom_teleop_latest.py
@@ -17,14 +17,17 @@ class MimicPose:
         self.arm = dvrk.psm(ral, arm_name)
 
         # Subscribe to the /joint_states topic
-        rospy.Subscriber('phantom/pose_surgeon_console', PoseStamped, self.pose_callback)
+        rospy.Subscriber(config_dict['pose_topic'], PoseStamped, self.pose_callback)
         rospy.Subscriber('phantom/button', OmniButtonEvent, self.button_callback)
     
         self.phantom_orientation = None
         self.phantom_position = None
         self.current_jaw_pose = None
+
+        # The grey button must be pressed once to start
+        self.was_in_clutch = True
+        self.clutch = True 
         
-        self.initalised_teleop = False
         self.scale = config_dict["scale"]
         self.jaw_step_size = config_dict["jaw_step_size"]
 
@@ -33,20 +36,35 @@ class MimicPose:
         self.phantom_position = msg.pose.position # position
 
     def button_callback(self, msg):
+
+        # As long as the grey button is pressed continuously, self.clutch will be true
         if msg.grey_button == 1:
-            self.current_jaw_pose += self.jaw_step_size
-            print("Opening Jaw")
-        elif msg.white_button == 1:
-            self.current_jaw_pose -= self.jaw_step_size
-            print("Closing Jaw")
+
+            if self.was_in_clutch==False:
+                rospy.loginfo("Clutch Pressed")
+            self.clutch = True
+        
+        # When the grey button is released, a message is passed msg.grey_button=0 
+        # self.clutch will be set to false
+        elif msg.grey_button == 0:
+            self.clutch = False
+
+
+        # TODO: Callback code to handle the jaw using white button
+        # if msg.grey_button == 1:
+        #     self.current_jaw_pose += self.jaw_step_size
+        #     print("Opening Jaw")
+        # elif msg.white_button == 1:
+        #     self.current_jaw_pose -= self.jaw_step_size
+        #     print("Closing Jaw")
         
         # self.arm.jaw.move_jp(self.current_jaw_pose)
 
 
 
-    def initialise_phantom_teleop(self):
+    def transition_to_enabled(self):
         '''
-        This function is called only once whenever the teleop starts
+        This function is called everytime the clutch is pressed
         - It saves the initial position (x,y,z) of the arm and phantom
         - It changes the orientation of the arm to match that of the phantom omni
         - We use arm.move_cp instead of servo_cp here
@@ -55,13 +73,12 @@ class MimicPose:
             print('No Phantom Omni Pose received yet')
             return
         
-        rospy.loginfo('Initialising Phantom Teleop')
+        rospy.loginfo('Clutch Released: Matching Phantom Omni Orientation')
         goal = PyKDL.Frame()
         self.initial_arm_pose = self.arm.setpoint_cp()
         self.current_jaw_pose = self.arm.jaw.setpoint_jp()
 
         self.initial_arm_position = self.initial_arm_pose.p
-
         self.initial_phantom_position = PyKDL.Vector(self.phantom_position.x, self.phantom_position.y, self.phantom_position.z)
 
         # Aligning the orientation of the arm with the phantom omni
@@ -70,11 +87,11 @@ class MimicPose:
         goal.M = PyKDL.Rotation.Quaternion(q[0],q[1],q[2],q[3])
 
         # Move the arm to the goal pose
-        print('Using move_cp to align the arm: Takes less than a minute | Please wait')
+        # print('Using move_cp to align the arm: Takes less than a minute | Please wait')
         self.arm.move_cp(goal).wait(True)
 
         self.initalised_teleop = True
-        print('Initialisation complete')
+        # print('Initialisation complete')
 
 
     def move_cartesian(self):
@@ -108,13 +125,25 @@ class MimicPose:
         #.wait()
 
     def run(self):
-        while not self.initalised_teleop:
-            self.initialise_phantom_teleop()
-            rospy.sleep(0.1)
-
+        # while not self.initalised_teleop:
+        #     self.initialise_phantom_teleop()
+        #     rospy.sleep(0.1)
+        rospy.loginfo("Hold the Pen in Position, Hold and Release the Grey Button Once to Start")
         while not rospy.is_shutdown():
             
-            self.move_cartesian()
+            # When clutch is not pressed
+            if not self.clutch:
+
+                if self.was_in_clutch:
+                    self.transition_to_enabled()
+                    self.was_in_clutch = False 
+
+                self.move_cartesian()
+
+            # When Clutch is pressed
+            elif self.clutch:
+                self.was_in_clutch = True
+
             rospy.sleep(0.1)
         # rospy.spin()
 
@@ -131,10 +160,12 @@ if __name__ == '__main__':
                         help='Scale for Translation')
     parser.add_argument('-j', '--jaw_step_size', type=float, default=0.1,
                     help='Jaw Step Size')
+    parser.add_argument('-p','--pose_topic',type=str,default="/phantom/pose",help="The pose to mimic")
     args = parser.parse_args(argv)
 
     config_dict = {"scale":args.scale,
-                   "jaw_step_size":args.jaw_step_size}
+                   "jaw_step_size":args.jaw_step_size,
+                   "pose_topic":args.pose_topic}
     
     ral = crtk.ral('mimic_pose')
     mimic_pose = MimicPose(ral, args.arm,config_dict)
