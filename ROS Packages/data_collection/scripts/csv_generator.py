ddb99fdb0bce3126af2371e7860ddb12c281a559
Chetan Reddy Narayanaswamy <chetanreddy1412@gmail.com>
2025-06-11 20:00:27 -0700

    data collection pipeline

--- ROS Packages/data_collection/scripts/csv_generator.py
+++ ROS Packages/data_collection/scripts/csv_generator.py
@@ -359,7 +359,9 @@ if __name__ == '__main__':
     rospy.init_node('csv_generator', anonymous=True)
 
     # LOGGING_FOLDER = rospy.get_param("LOGGING_FOLDER")
-    LOGGING_FOLDER = rospy.get_param("LOGGING_FOLDER", "/home/stanford/catkin_ws/src/Autonomous-Surgical-Robot-Data/Collaborative Three Handed")
+    # specify the right LOGGING_FOLDER here:
+    LOGGING_FOLDER = "/home/stanford/catkin_ws/src/Autonomous-Surgical-Robot-Data/Collaborative Three Handed"
+    LOGGING_FOLDER = rospy.get_param("LOGGING_FOLDER", LOGGING_FOLDER) # This reads the logging_folder from a ros parameter if available
     # List of topics and their message types
     argv = crtk.ral.parse_argv(sys.argv[1:])  # Skip argv[0], script name
 
